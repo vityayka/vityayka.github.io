469c96a559ca8671a277ec57c9fccd8adc306148
vityayka <vityayka94@gmail.com>
2016-05-11 13:04:03 +0300

    Hotfixes for pingbuller

--- GoIT/markup/home/exam/script.js
+++ GoIT/markup/home/exam/script.js
@@ -1,7 +1,10 @@
 $(function() {
     $('.jcarousel').jcarousel({
         // Configuration goes here
-        wrap: 'circular'
+        wrap: 'circular',
+        transitions: {
+          transforms: true
+        }
     });
     // $('.jcarousel').jcarousel('scroll', '+=1');
 
@@ -12,24 +15,24 @@ $(function() {
     $('.jcarousel-next').jcarouselControl({
         target: '+=1'
     });
-    $('.jcarousel').jcarouselAutoscroll({
-      interval: 1000,
-      target: '+=1',
-      autostart: true
-  });
+    // $('.jcarousel').jcarouselAutoscroll({
+      // interval: 5000,
+      // target: '+=1',
+      // autostart: true
+  // });
      $('.jcarousel-pagination')
        .on('jcarouselpagination:active', 'a', function() {
-           $(this).addClass('active');
+           $(this).addClass('isActive');
        })
        .on('jcarouselpagination:inactive', 'a', function() {
-           $(this).removeClass('active');
+           $(this).removeClass('isActive');
        })
        .on('click', function(e) {
            e.preventDefault();
        })
        .jcarouselPagination({
            item: function(page) {
-               return '<a href="#' + page + '">' + page + '</a>';
+               return '<a href="#' + page + '"></a>';
            }
        });
 });
