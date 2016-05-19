e400430e2ad45063c188f3b824352f53ef336067
vityayka <vityayka94@gmail.com>
2016-05-19 13:00:08 +0300

    replace

---
+++ pingbuller/script.js
@@ -0,0 +1,37 @@
+$(function() {
+    $('.jcarousel').jcarousel({
+        wrap: 'circular',
+        transitions: {
+          transforms: true
+        }
+    });
+    $('.jcarousel-prev').jcarouselControl({
+        target: '-=1'
+    });
+
+    $('.jcarousel-next').jcarouselControl({
+        target: '+=1'
+    });
+
+    $('.jcarousel').jcarouselAutoscroll({
+      interval: 5000,
+      target: '+=1',
+      autostart: true
+    }); 
+
+     $('.jcarousel-pagination')
+       .on('jcarouselpagination:active', 'a', function() {
+           $(this).addClass('isActive');
+       })
+       .on('jcarouselpagination:inactive', 'a', function() {
+           $(this).removeClass('isActive');
+       })
+       .on('click', function(e) {
+           e.preventDefault();
+       })
+       .jcarouselPagination({
+           item: function(page) {
+               return '<a href="#' + page + '"></a>';
+           }
+       });
+});
